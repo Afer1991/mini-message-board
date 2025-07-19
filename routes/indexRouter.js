ea5e5b79f4e3b254038b3fc4f3247c22e022e1ca
Af1991 <152735889+Afer1991@users.noreply.github.com>
2025-07-19 17:39:11 -0400

    Add feat to display messages on homepage

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,23 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
 const { Router } = require("express");
 
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { messages: messages });
 });
 
 module.exports = indexRouter;
