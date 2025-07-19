096a5dfe1ccf2d1a3b38b32f562e5c908f4b2095
Af1991 <152735889+Afer1991@users.noreply.github.com>
2025-07-19 14:33:15 -0400

    Add new route

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use("/", indexRouter);
+app.use("/new", newRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
