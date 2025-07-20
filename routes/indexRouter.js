bbf27fc9f49804d685c76e0fe002733ad9d7f57f
Af1991 <152735889+Afer1991@users.noreply.github.com>
2025-07-19 20:34:18 -0400

    Add post function to submit new message

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -20,4 +20,9 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
 
+indexRouter.post("/new", (req, res) => {
+  messages.push({ text: req.body.message, user: req.body.username, added: new Date() });
+  res.redirect("/");
+});
+
 module.exports = indexRouter;
